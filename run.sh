75c632f8fb2aaa26572227eb9dd5c40d6595447f
kgtkr <contact@kgtkr.net>
2023-05-30 14:14:34 +0900

    linux対応

--- run.sh
+++ run.sh
@@ -5,15 +5,32 @@ javac net/kgtkr/seekprog/App.java
 
 processing-java --sketch=$PWD/Pde --output=$PWD/PdeOut --force --build
 cp PdeOut/*.class .
-# mac only
+
 classpath="."
-for jar in /Applications/Processing.app/Contents/Java/*.jar; do
-    classpath="$classpath:$jar"
-done
-for jar in /Applications/Processing.app/Contents/Java/core/library/*.jar; do
-    classpath="$classpath:$jar"
-done
-for jar in /Applications/Processing.app/Contents/Java/modes/java/mode/*.jar; do
-    classpath="$classpath:$jar"
-done
+
+if [ "$(uname)" == 'Linux' ]; then
+    BASEDIR=$(dirname $(which processing-java))
+    for jar in $BASEDIR/lib/*.jar; do
+        classpath="$classpath:$jar"
+    done
+    for jar in $BASEDIR/core/library/*.jar; do
+        classpath="$classpath:$jar"
+    done
+    for jar in $BASEDIR/modes/java/mode/*.jar; do
+        classpath="$classpath:$jar"
+    done
+fi
+
+if [ "$(uname)" == 'Darwin' ]; then
+    for jar in /Applications/Processing.app/Contents/Java/*.jar; do
+        classpath="$classpath:$jar"
+    done
+    for jar in /Applications/Processing.app/Contents/Java/core/library/*.jar; do
+        classpath="$classpath:$jar"
+    done
+    for jar in /Applications/Processing.app/Contents/Java/modes/java/mode/*.jar; do
+        classpath="$classpath:$jar"
+    done
+fi
+
 java -cp "$classpath" net.kgtkr.seekprog.App
